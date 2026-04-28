fb71ab42718f92ee9c8b005ad756cc7f49436dd8
MikCas <mikhail.cassar@gmail.com>
2026-04-28 23:15:21 +0200

    feat(scene): add Scene class skeleton

---
+++ include/Scene.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Camera.h"
+#include "Material.h"
+#include "Object.h"
+#include "Vec3.h"
+
+#include <memory>
+
+class Scene {
+public:
+    ObjectList world;
+    Camera     camera;
+
+    Scene(const Camera& cam);
+
+    void addSphere(const Vec3& center, double radius, std::shared_ptr<material> mat);
+
+    std::shared_ptr<Lambertian> lambertian(const Vec3& albedo);
+    std::shared_ptr<Metal> metal(const Vec3& albedo, double fuzz);
+    std::shared_ptr<Dielectric> dielectric(double refractiveIndex);
+}
